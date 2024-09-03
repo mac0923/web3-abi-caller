9a20f2c00a10ddde895e565ba0fbedb75c4d47c8
mac0923 <aerymzc9123@gmail.com>
2024-09-03 10:34:29 +0800

    feat: add local switch button

--- src/components/LocalesButton/LocalesButton.tsx
+++ src/components/LocalesButton/LocalesButton.tsx
@@ -1,3 +1,25 @@
+import { useI18nLocalStorage } from '@/lib/i18n'
+
 export default function LocalesButton() {
-  return <>Locales Button</>
+  const [localeState, setLocale] = useI18nLocalStorage()
+
+  function updateLocal() {
+    if (localeState === 'zh') {
+      setLocale('en')
+    }
+    if (localeState === 'en') {
+      setLocale('zh')
+    }
+  }
+
+  return (
+    <>
+      <div
+        onClick={updateLocal}
+        className="flex items-center justify-center border-slate-300 border-[1px] rounded-[4px] text-[10px] w-[20px] h-[20px] cursor-pointer hover:text-sky-200"
+      >
+        {localeState === 'zh' ? '中' : 'EN'}
+      </div>
+    </>
+  )
 }
